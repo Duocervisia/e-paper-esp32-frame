416a0f743539f6d61f5079227946d8122aa704ca
TreeSparrow69 <treesparrow69@protonmail.com>
2024-02-27 13:39:23 +0100

    hiilfe

--- e-paper-esp32-frame.ino
+++ e-paper-esp32-frame.ino
@@ -1,12 +1,23 @@
-#define LED_BUILTIN 2 // Define built-in LED pin for ESP32
+#define button 15 // Define the button pin
+#define led 2 // Define the LED pin
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT); // Initialize digital pin as an output.
+void setup() 
+{
+  delay(500); // Wait for 0,5 seconds
+  pinMode(button, INPUT); // Set the button pin as input
+  pinMode(led, OUTPUT); // Set the LED pin as output
+  Serial.begin(115200); // Initialize serial communication at 115200 bits per second
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on (HIGH is the voltage level)
-  delay(1000); // Wait for a second
-  digitalWrite(LED_BUILTIN, LOW); // Turn the LED off by making the voltage LOW
-  delay(1000); // Wait for a second
+void loop() 
+{
+  int buttonState = analogRead(button); // Read the button state
+  if (buttonState > 4000) // If the button is pressed
+  {
+    digitalWrite(led, HIGH); // Turn on the LED
+  }
+  else // If the button is not pressed
+  {
+    digitalWrite(led, LOW); // Turn off the LED
+  }
 }
