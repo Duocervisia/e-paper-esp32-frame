dec5c9cda9062a1e99a6cf040404a84b35761136
TreeSparrow69 <treesparrow69@protonmail.com>
2024-02-28 16:33:13 +0100

    test

--- e-paper-esp32-frame.ino
+++ e-paper-esp32-frame.ino
@@ -1,23 +1,26 @@
-#define button 15 // Define the button pin
-#define led 2 // Define the LED pin
+#include "esp_sleep.h"
+#define LED_PIN 2 // Change this to the GPIO pin connected to your LED
 
-void setup() 
-{
-  delay(500); // Wait for 0,5 seconds
-  pinMode(button, INPUT); // Set the button pin as input
-  pinMode(led, OUTPUT); // Set the LED pin as output
-  Serial.begin(115200); // Initialize serial communication at 115200 bits per second
+RTC_DATA_ATTR int bootCount = 0;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT); // Set the LED pin as output
+
+  Serial.begin(115200);
+  delay(1000); // Allow time for serial communication to initialize
+
+  // Increase boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  if (bootCount == 1) {
+    Serial.println("Going into deep sleep now");
+    esp_deep_sleep(1000000LL * 20); // Sleep for 20 seconds
+  } else {
+    digitalWrite(LED_PIN, HIGH); // Turn on the LED
+  }
 }
 
-void loop() 
-{
-  int buttonState = analogRead(button); // Read the button state
-  if (buttonState > 4000) // If the button is pressed
-  {
-    digitalWrite(led, HIGH); // Turn on the LED
-  }
-  else // If the button is not pressed
-  {
-    digitalWrite(led, LOW); // Turn off the LED
-  }
+void loop() {
+  // This will never be reached, as the ESP32 will go into deep sleep in the setup() function
 }
