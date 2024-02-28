891c90dd6c1d5b88c5b6ef936c36d9b03d68d672
Tobias Henning <henningt@htw-berlin.de>
2024-02-28 10:50:16 +0100

    fixed jero code fixe

--- e-paper-esp32-frame.ino
+++ e-paper-esp32-frame.ino
@@ -1,23 +1,25 @@
-#define button 15 // Define the button pin
+#define button 12 // Define the button pin
 #define led 2 // Define the LED pin
 
 void setup() 
 {
-  delay(500); // Wait for 0,5 seconds
-  pinMode(button, INPUT); // Set the button pin as input
+  // delay(500); // Wait for 0,5 seconds
+  pinMode(button,INPUT_PULLUP);
+  // pinMode(button, OUTPUT); // Set the button pin as input
   pinMode(led, OUTPUT); // Set the LED pin as output
   Serial.begin(115200); // Initialize serial communication at 115200 bits per second
 }
 
 void loop() 
 {
-  int buttonState = analogRead(button); // Read the button state
-  if (buttonState > 4000) // If the button is pressed
+  int buttonState = digitalRead(button); // Read the button state
+  Serial.println(buttonState); // Print the button state
+  if (buttonState == HIGH) // If the button is pressed
   {
-    digitalWrite(led, HIGH); // Turn on the LED
+    digitalWrite(led, LOW); // Turn on the LED
   }
   else // If the button is not pressed
   {
-    digitalWrite(led, LOW); // Turn off the LED
+    digitalWrite(led, HIGH); // Turn off the LED
   }
 }
